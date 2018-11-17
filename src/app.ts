1d486fe41401ecec2afcd53a7e00709632ceb3f8
take8 <37664118+take8@users.noreply.github.com>
2018-11-18 01:27:07 +0900

    Convert to TypeScript

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
