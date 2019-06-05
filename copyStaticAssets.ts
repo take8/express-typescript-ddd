13a2e84bc69853d79e866c2567d78d8161331be9
take8 <37664118+take8@users.noreply.github.com>
2019-06-05 22:11:50 +0900

    Fix stylesheet path

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,6 +1,6 @@
 import * as shell from "shelljs";
 
 shell.cp("-R", "src/public/js/lib", "dist/public/js/");
-shell.cp("-R", "src/public/stylesheets", "dist/public/");
+shell.cp("-R", "src/public/stylesheets", "dist/public/css");
 // shell.cp("-R", "src/public/fonts", "dist/public/");
 shell.cp("-R", "src/public/images", "dist/public/");
