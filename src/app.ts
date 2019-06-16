7c7c553ccf92964cf1ae0a390547ec7835cb5f24
take8 <37664118+take8@users.noreply.github.com>
2019-06-16 12:30:51 +0900

    Rename directory of view template

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import routes from "./presentation/routes";
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "../views"));
+app.set("views", path.join(__dirname, "../templates"));
 app.set("view engine", "pug");
 
 // logger
