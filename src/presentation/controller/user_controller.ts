c7e909bfd964c268edfda208ab2da4147669ebc8
take8 <37664118+take8@users.noreply.github.com>
2019-06-18 16:01:57 +0900

    Add mock of user new, edit, show

--- src/presentation/controller/user_controller.ts
+++ src/presentation/controller/user_controller.ts
@@ -1,8 +1,29 @@
 import { Request, Response, NextFunction } from "express";
 
 /**
- * GET users listing.
+ * List users.
  */
 export let index = (req: Request, res: Response, next: NextFunction) => {
   res.render("user/index", { title: "Users" });
+};
+
+/**
+ * Show user.
+ */
+export let show = (req: Request, res: Response, next: NextFunction) => {
+  res.render("user/show", { title: "User: xxxx" });
+};
+
+/**
+ * Render user form to create.
+ */
+export let _new = (req: Request, res: Response, next: NextFunction) => {
+  res.render("user/new", { title: "New User" });
+};
+
+/**
+ * Render user form to update.
+ */
+export let edit = (req: Request, res: Response, next: NextFunction) => {
+  res.render("user/edit", { title: "User: yyyy" });
 };
